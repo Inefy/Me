46bc98e3f67820887f2033e4289d820b3de1ad15
Inefy <zbatten@mun.ca>
2023-05-03 18:27:09 -0230

    Have shapes working and brush styles

--- src/components/paintapp/BrushStylePicker.jsx
+++ src/components/paintapp/BrushStylePicker.jsx
@@ -1,26 +1,33 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPencilAlt, faPaintBrush, faSprayCan, faHighlighter, faEraser } from '@fortawesome/free-solid-svg-icons';
 import './BrushStylePicker.css';
 
 const brushStyles = [
-  { id: 'pencil', name: 'Pencil' },
-  { id: 'brush', name: 'Brush' },
-  { id: 'spray', name: 'Spray' },
+  { id: 'pencil', name: 'Pencil', icon: faPencilAlt },
+  { id: 'brush', name: 'Brush', icon: faPaintBrush },
+  { id: 'spray', name: 'Spray', icon: faSprayCan },
+  { id: 'marker', name: 'Marker', icon: faHighlighter },
+  { id: 'eraser', name: 'Eraser', icon: faEraser },
 ];
 
 const BrushStylePicker = ({ onChange }) => {
-  const handleBrushStyleChange = (event) => {
-    onChange(event.target.value);
+  const handleBrushStyleChange = (brushStyle) => {
+    onChange(brushStyle);
   };
 
   return (
     <div className="brush-style-picker">
-      <select onChange={handleBrushStyleChange}>
-        {brushStyles.map((style) => (
-          <option key={style.id} value={style.id}>
-            {style.name}
-          </option>
-        ))}
-      </select>
+      {brushStyles.map((style) => (
+        <button
+          key={style.id}
+          className="brush-style-button"
+          onClick={() => handleBrushStyleChange(style.id)}
+          title={style.name}
+        >
+          <FontAwesomeIcon icon={style.icon} />
+        </button>
+      ))}
     </div>
   );
 };
--- src/components/paintapp/DrawingCanvas.jsx
+++ src/components/paintapp/DrawingCanvas.jsx
@@ -1,6 +1,10 @@
 import React, { useRef, useState, useEffect } from 'react';
-import { Stage, Layer, Line } from 'react-konva';
+import { Stage, Layer, Line, Circle, Rect, Star, Path } from 'react-konva';
+import Konva from 'konva';
 import './DrawingCanvas.css';
+
+
+
 
 const DrawingCanvas = ({
   selectedColor,
@@ -10,6 +14,27 @@ const DrawingCanvas = ({
 }) => {
   const [lines, setLines] = useState([]);
   const isDrawing = useRef(false);
+  const [shapes, setShapes] = useState([]);
+  const stageRef = useRef(null);
+  const layerRef = useRef(null);
+  const [canvasSize, setCanvasSize] = useState({ width: window.innerWidth * 0.8, height: window.innerHeight * 0.8 });
+
+  const addShape = (newShape) => {
+    setShapes((prevShapes) => [...prevShapes, newShape]);
+  };
+
+
+  useEffect(() => {
+    const handleResize = () => {
+      setCanvasSize({ width: window.innerWidth * 0.8, height: window.innerHeight * 0.8 });
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   const handleMouseMove = (event) => {
     if (!isDrawing.current) {
@@ -17,79 +42,134 @@ const DrawingCanvas = ({
     }
     const stage = event.target.getStage();
     const pointerPosition = stage.getPointerPosition();
-    setLines((currentLines) => {
-      const lastLine = currentLines[currentLines.length - 1];
-      return [
-        ...currentLines.slice(0, -1),
-        {
-          ...lastLine,
-          points: [...lastLine.points, pointerPosition.x, pointerPosition.y],
-        },
-      ];
-    });
-  };
 
-  const handleObjectClick = (event, index) => {
-    if (event.evt.button !== 0) {
-      return;
+    const addPoints = (x, y) => {
+      setLines((currentLines) => {
+        const lastLine = currentLines[currentLines.length - 1];
+        return [
+          ...currentLines.slice(0, -1),
+          {
+            ...lastLine,
+            points: [...lastLine.points, x, y],
+          },
+        ];
+      });
+    };
+
+
+    switch (selectedBrushStyle) {
+      case 'spray':
+        for (let i = 0; i < 30; i++) {
+          const offsetX = (Math.random() * 2 - 1) * selectedThickness;
+          const offsetY = (Math.random() * 2 - 1) * selectedThickness;
+          addPoints(pointerPosition.x + offsetX, pointerPosition.y + offsetY);
+        }
+        break;
+      default:
+        addPoints(pointerPosition.x, pointerPosition.y);
+        break;
     }
-    setSelectedObject(shape);
-    event.cancelBubble = true;
   };
 
-  const [selectedObject, setSelectedObject] = useState(null);
+  const handleMouseDown = (event) => {
+    if (selectedShape) {
+      const stage = event.target.getStage();
+      const pointerPosition = stage.getPointerPosition();
+      const newShapeProps = {
+        x: pointerPosition.x,
+        y: pointerPosition.y,
+        fill: selectedColor,
+        draggable: true,
+      };
 
-  useEffect(() => {
-    if (isDrawing.current) {
-      onNewObjectAdded();
-      setLines((currentLines) => [
-        ...currentLines,
+      switch (selectedShape) {
+        case 'circle':
+          addShape({ type: 'circle', ...newShapeProps, radius: selectedThickness });
+          break;
+        case 'square':
+          addShape({ type: 'rect', ...newShapeProps, width: selectedThickness * 2, height: selectedThickness * 2 });
+          break;
+        case 'star':
+          addShape({
+            type: 'star',
+            ...newShapeProps,
+            numPoints: 5,
+            innerRadius: selectedThickness,
+            outerRadius: selectedThickness * 2,
+          });
+          break;
+        case 'heart':
+          addShape({
+            type: 'path',
+            ...newShapeProps,
+            data: 'M5.34,0C2.39,0,0,2.39,0,5.34c0,4.23,5.34,8.52,5.34,8.52S10.67,9.57,10.67,5.34C10.67,2.39,8.29,0,5.34,0z',
+            scale: { x: selectedThickness / 5, y: selectedThickness / 5 },
+          });
+          break;
+        default:
+          break;
+      }
+    } else {
+      isDrawing.current = true;
+      setLines([
+        ...lines,
         {
-          color,
-          thickness,
+          color: selectedColor,
+          thickness: parseFloat(selectedThickness),
           points: [],
-          brushStyle,
         },
       ]);
     }
-  }, [brushStyle, color, thickness, onNewObjectAdded]);
+  };
+
 
   return (
     <div className="drawing-canvas">
       <Stage
-        onClick={() => setSelectedObject(null)}
-        width={window.innerWidth}
-        height={window.innerHeight}
-        onMouseDown={() => {
-          isDrawing.current = true;
-        }}
+        ref={stageRef}
+        width={canvasSize.width}
+        height={canvasSize.height}
+        onMouseDown={handleMouseDown}
         onMouseUp={() => {
           isDrawing.current = false;
-          onDrawingEnd();
         }}
         onMouseMove={handleMouseMove}
-
-        onTouchStart={() => {
-          isDrawing.current = true;
-        }}
+        onTouchStart={handleMouseDown}
         onTouchEnd={() => {
           isDrawing.current = false;
-          onDrawingEnd();
         }}
         onTouchMove={handleMouseMove}
       >
-        <Layer>
+        <Layer ref={layerRef}>
           {lines.map((line, i) => (
             <Line
-            key={i}
-            points={line.points}
-            stroke={line.color}
-            strokeWidth={line.thickness}
-            draggable
-            onClick={(e) => handleObjectClick(e, i)}
-          />
+              key={i}
+              points={line.points}
+              stroke={line.color}
+              strokeWidth={line.thickness}
+              tension={0.5}
+              lineCap="round"
+              globalCompositeOperation={
+                selectedBrushStyle === 'eraser' ? 'destination-out' : 'source-over'
+              }
+            />
           ))}
+          {shapes.map((shape, i) => {
+            switch (shape.type) {
+              case 'circle':
+                return <Circle key={`shape-${i}`} {...shape} />;
+              case 'rect':
+                return <Rect key={`shape-${i}`} {...shape} />;
+              case 'star':
+                return <Star key={`shape-${i}`} {...shape} />;
+              case 'path':
+                return <Path key={`shape-${i}`} {...shape} />;
+              default:
+                return null;
+            }
+          })}
         </Layer>
+
       </Stage>
     </div>
   );
